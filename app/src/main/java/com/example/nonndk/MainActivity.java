70286ebf446f8f8b66cbcc6ff6697620a572fd08
Abraham Qian <qianyonglian@outlook.com>
2020-08-13 11:21:00 +0800

    tweak0813

--- app/src/main/java/com/example/nonndk/MainActivity.java
+++ app/src/main/java/com/example/nonndk/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nonndk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCenter.start(getApplication(), "6fcb5289-d7e5-4ada-a6d8-6154368ab279", Analytics.class, Crashes.class);
+AppCenter.setLogLevel(Log.VERBOSE);
 
 TextView tv=new TextView(this);
 tv.setText("Version4, Hello World form backend");
