ffc9fbe85aa774dc2e709ac7a700c819553d3f9d
Abraham Qian <qianyonglian@outlook.com>
2020-08-13 13:39:26 +0800

    add another activity

---
+++ app/src/main/java/com/example/nonndk/DisplayMessageActivity.java
@@ -0,0 +1,30 @@
+package com.example.nonndk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.microsoft.appcenter.crashes.Crashes;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Crashes.generateTestCrash();
+
+
+        setContentView(R.layout.activity_display_message);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
--- app/src/main/java/com/example/nonndk/MainActivity.java
+++ app/src/main/java/com/example/nonndk/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.nonndk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
@@ -12,15 +15,21 @@ import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE="com.example.NonNDK.Message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCenter.start(getApplication(), "6fcb5289-d7e5-4ada-a6d8-6154368ab279", Analytics.class, Crashes.class);
 AppCenter.setLogLevel(Log.VERBOSE);
 
-TextView tv=new TextView(this);
-tv.setText("Version4, Hello World form backend");
-        setContentView(tv);
-       Crashes.generateTestCrash();
+        setContentView(R.layout.activity_main);
+
+    }
+    public void sendMessage(View view){
+Intent intent=new Intent(this,DisplayMessageActivity.class);
+        EditText editText=(EditText)findViewById(R.id.editText);
+        String message=editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 }
